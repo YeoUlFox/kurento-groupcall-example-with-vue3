b447aaf5aa9abf8e34cdef2034715a16c30f15af
YeoUl <41969902+YeoUlFox@users.noreply.github.com>
2022-07-25 10:34:55 +0900

    add spring boot server

---
+++ kurento-demo-sprintboot-server/src/test/java/com/yeoul/kurento_demo/KurentoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeoul.kurento_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurentoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
